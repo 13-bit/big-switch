d7f02296c622df845f7d91e3663287c363709f06
Peter Roe <pete@13bit.me>
2018-07-11 17:47:26 -0500

    Basic color fade

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
-#include <Arduino.h>
+#include <WS2812FX.h>
+
+#define LED_COUNT 5
+#define LED_PIN PIN_D5
+
+WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(9600);
+  ws2812fx.init();
+  ws2812fx.setBrightness(100);
+  ws2812fx.setSpeed(50);
+  ws2812fx.setMode(FX_MODE_RAINBOW_CYCLE);
+  ws2812fx.start();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  ws2812fx.service();
+  // delay(100);
 }
