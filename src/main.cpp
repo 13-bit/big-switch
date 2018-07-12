2995f8177bef1274050cc2f3d6349ffdf9c8a9d2
Peter Roe <pete@13bit.me>
2018-07-11 20:17:50 -0500

    Added off mode

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #define LED_PIN PIN_D5
 #define BUTTON_PIN PIN_B0
 
-#define FX_NUM_MODES 4
+#define FX_NUM_MODES 5
 
 WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 Bounce debouncer = Bounce();
@@ -14,10 +14,15 @@ struct ledFxMode {
   uint8_t mode;
   uint32_t color;
   uint16_t speed;
+  uint8_t brightness;
 };
 
 uint8_t fxIndex = 0;
-ledFxMode fxModes[FX_NUM_MODES] = {{FX_MODE_RAINBOW_CYCLE, 0x000000, 8192}, {FX_MODE_BREATH, 0xdd550c, 512}, {FX_MODE_COLOR_WIPE_RANDOM, 0x000000, 256}, {FX_MODE_FIREWORKS_RANDOM, 0x000000, 64}};
+ledFxMode fxModes[FX_NUM_MODES] = {{FX_MODE_RAINBOW_CYCLE, 0x000000, 8192, 255},
+                                  {FX_MODE_BREATH, 0xdd550c, 512, 255},
+                                  {FX_MODE_COLOR_WIPE_RANDOM, 0x000000, 256, 25},
+                                  {FX_MODE_FIREWORKS_RANDOM, 0x000000, 64, 25},
+                                  {FX_MODE_STATIC, 0x000000, 0, 0}};
 
 boolean isOn = true;
 
@@ -28,13 +33,14 @@ void changeMode() {
   ws2812fx.setSpeed(mode.speed);
   ws2812fx.setColor(mode.color);
   ws2812fx.setMode(mode.mode);
+  ws2812fx.setBrightness(mode.brightness);
 }
 
 void setup() {
   ledFxMode mode = fxModes[fxIndex % FX_NUM_MODES];
 
   ws2812fx.init();
-  ws2812fx.setBrightness(255);
+  ws2812fx.setBrightness(mode.brightness);
   ws2812fx.setSpeed(mode.speed);
   ws2812fx.setColor(mode.color);
   ws2812fx.setMode(mode.mode);
